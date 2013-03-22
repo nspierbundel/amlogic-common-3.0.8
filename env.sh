879bf6fe32d431f39c64e08c332ad97cc8453975
nspierbundel <nspierbundel@hotmail.com>
2013-03-22 16:47:12 +0000

    [go2] audio fix test

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@ export CROSS_COMPILE=/data/buildroot-AmLogic/output/external-toolchain/bin/arm-n
 #export CROSS_COMPILE=/data/toolchain/arm-2010.09/bin/arm-none-linux-gnueabi-
 
 make clean
-#make meson6_atv1200_defconfig
+make meson6_atv1200_defconfig
 
 #make menuconfig
 #make vmlinux
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Mar 22 14:49:14 UTC 2013"
+#define UTS_VERSION "#3 SMP PREEMPT Fri Mar 22 16:44:43 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
