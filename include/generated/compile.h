a89b959a1265178725e77cd6e7bda08d52acf08c
nspierbundel <nspierbundel@hotmail.com>
2013-03-25 12:41:08 +0000

    [go2] hdmi fix

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 7 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 SMP PREEMPT Mon Mar 25 10:29:10 UTC 2013"
+#define UTS_VERSION "#7 SMP PREEMPT Mon Mar 25 12:38:18 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
