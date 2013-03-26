3b3707f5bd2d3babee42501ed2660161b532d672
nspierbundel <nspierbundel@hotmail.com>
2013-03-26 13:20:13 +0000

    [go2] drivers added

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 11 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 SMP PREEMPT Mon Mar 25 20:51:21 UTC 2013"
+#define UTS_VERSION "#11 SMP PREEMPT Tue Mar 26 13:18:17 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
