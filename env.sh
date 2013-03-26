ac9a2a9b57575ecf7db82d7d93a13c25bf04247d
nspierbundel <nspierbundel@hotmail.com>
2013-03-26 14:22:52 +0000

    [G02] enabled eth

--- env.sh
+++ env.sh
@@ -2,10 +2,10 @@ export ARCH=arm
 export CROSS_COMPILE=/data/buildroot-AmLogic/output/external-toolchain/bin/arm-none-linux-gnueabi-
 #export CROSS_COMPILE=/data/toolchain/arm-2010.09/bin/arm-none-linux-gnueabi-
 
-#make clean
-make meson6_atv1200_defconfig
+make clean
+#make meson6_atv1200_defconfig
 
-#make menuconfig
+make menuconfig
 #make vmlinux
 
 TOP=${PWD}
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 11 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#11 SMP PREEMPT Tue Mar 26 13:18:17 UTC 2013"
+#define UTS_VERSION "#12 SMP PREEMPT Tue Mar 26 14:01:03 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
