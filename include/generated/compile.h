325318963f11e5ccbd1ea9038091c66194bc9ded
nspierbundel <nspierbundel@hotmail.com>
2013-03-27 21:19:33 +0000

    [G02] added gpio test functions

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 21 */
+/* This file is auto generated, version 22 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#21 SMP PREEMPT Wed Mar 27 15:53:00 UTC 2013"
+#define UTS_VERSION "#22 SMP PREEMPT Wed Mar 27 20:40:40 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
