b0460b72e0df4a25f9e7801fd72902866f4a509e
nspierbundel <nspierbundel@hotmail.com>
2013-03-27 08:50:46 +0000

    [go2] test for i2c

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 13 */
+/* This file is auto generated, version 14 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#13 SMP PREEMPT Tue Mar 26 21:02:39 UTC 2013"
+#define UTS_VERSION "#14 SMP PREEMPT Wed Mar 27 08:27:42 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
