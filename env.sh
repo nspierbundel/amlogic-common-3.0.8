44ad36c8d7edd8ef7123c2ec6fea51c7efa3f21c
nspierbundel <nspierbundel@hotmail.com>
2013-03-27 10:41:27 +0000

    [G02] working on usb drivers

--- env.sh
+++ env.sh
@@ -2,14 +2,15 @@ export ARCH=arm
 export CROSS_COMPILE=/data/buildroot-AmLogic/output/external-toolchain/bin/arm-none-linux-gnueabi-
 #export CROSS_COMPILE=/data/toolchain/arm-2010.09/bin/arm-none-linux-gnueabi-
 
-#make clean
-#make meson6_atv1200_defconfig
+make clean
+make meson6_atv1200_defconfig
 
-#make menuconfig
+make menuconfig
+cp .config arch/arm/configs/meson6_atv1200_defconfig
 #make vmlinux
 
 TOP=${PWD}
 export MKIMAGE=${TOP}/arch/arm/boot/mkimage
-#make uImage -j16
+make uImage -j16
 #make uImage
-make modules  -j16
+#make modules  -j16
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 14 */
+/* This file is auto generated, version 16 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#14 SMP PREEMPT Wed Mar 27 08:27:42 UTC 2013"
+#define UTS_VERSION "#16 SMP PREEMPT Wed Mar 27 10:38:58 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
