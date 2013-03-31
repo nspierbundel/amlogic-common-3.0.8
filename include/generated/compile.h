08992f72a7d291d83c9c5b0eda80ca66fc54e4d2
nspierbundel <nspierbundel@hotmail.com>
2013-03-31 21:50:29 +0000

    [G02] changed mali driver

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 29 */
+/* This file is auto generated, version 31 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#29 SMP PREEMPT Sun Mar 31 12:03:07 UTC 2013"
+#define UTS_VERSION "#31 SMP PREEMPT Sun Mar 31 21:46:18 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
