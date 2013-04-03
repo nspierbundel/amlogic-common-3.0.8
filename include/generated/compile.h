bd9c7c0bd41bb948e900421dc97464ef6915a9ec
nspierbundel <nspierbundel@hotmail.com>
2013-04-03 15:13:56 +0000

    [mali] rev14 filles now added to git

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 39 */
+/* This file is auto generated, version 40 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#39 SMP PREEMPT Wed Apr 3 13:48:26 UTC 2013"
+#define UTS_VERSION "#40 SMP PREEMPT Wed Apr 3 15:05:28 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
