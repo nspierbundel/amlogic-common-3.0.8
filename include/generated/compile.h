e2a6e951cdf32b8eafb8a1e7dd41992ccbc19c6f
nspierbundel <nspierbundel@hotmail.com>
2013-04-06 20:09:22 +0000

    [G02] changed board file

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 46 */
+/* This file is auto generated, version 47 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#46 SMP PREEMPT Sat Apr 6 11:28:40 UTC 2013"
+#define UTS_VERSION "#47 SMP PREEMPT Sat Apr 6 20:03:14 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
