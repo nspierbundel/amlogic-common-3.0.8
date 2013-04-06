95cf3891abe645183c8932fcbd009f7d6d4849fa
nspierbundel <nspierbundel@hotmail.com>
2013-04-06 20:48:25 +0000

    solved compiling issues

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 47 */
+/* This file is auto generated, version 48 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#47 SMP PREEMPT Sat Apr 6 20:03:14 UTC 2013"
+#define UTS_VERSION "#48 SMP PREEMPT Sat Apr 6 20:47:22 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
