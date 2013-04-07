7423abc25fe28493ccd4cc0a0055e0d7a14ef89a
nspierbundel <nspierbundel@hotmail.com>
2013-04-07 11:08:58 +0000

    [G02] initial commit mygicca 520

--- env.sh
+++ env.sh
@@ -2,11 +2,11 @@ export ARCH=arm
 export CROSS_COMPILE=/data/buildroot-AmLogic/output/external-toolchain/bin/arm-none-linux-gnueabi-
 #export CROSS_COMPILE=/data/toolchain/arm-2010.09/bin/arm-none-linux-gnueabi-
 
-make clean
+#make clean
 #make meson6_atv1200_defconfig
 #make meson_stv_mbx_mc_atv1200_defconfig
 #make menuconfig
-#cp .config arch/arm/configs/meson6_atv1200_defconfig
+cp .config arch/arm/configs/meson6_atv1200_defconfig
 #cp .config arch/arm/configs/meson_stv_mbx_mc_atv1200_defconfig
 #make vmlinux
 
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 48 */
+/* This file is auto generated, version 50 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#48 SMP PREEMPT Sat Apr 6 20:47:22 UTC 2013"
+#define UTS_VERSION "#50 SMP PREEMPT Sun Apr 7 08:21:02 UTC 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
