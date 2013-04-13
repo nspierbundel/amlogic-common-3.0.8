d792c160684780fc2121e2431b77487e53fe9a22
nspierbundel <nspierbundel@hotmail.com>
2013-04-13 19:44:05 +0000

    [test] test

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@ export CROSS_COMPILE=/data/buildroot-AmLogic/output/external-toolchain/bin/arm-n
 #export CROSS_COMPILE=/data/toolchain/arm-2010.09/bin/arm-none-linux-gnueabi-
 
 #make clean
-make meson6_atv1200_defconfig
+#make meson6_atv1200_defconfig
 #make meson_stv_mbx_mc_atv1200_defconfig
 #make menuconfig
 #cp .config arch/arm/configs/meson6_atv1200_defconfig
