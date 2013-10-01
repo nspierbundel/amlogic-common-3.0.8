9d9dfcc3be073f26f0d42e7144e5868db6cf5da5
nspierbundel <nspierbundel@hotmail.com>
2013-10-01 06:49:11 +0000

    bumped mali to v17

--- drivers/amlogic/mali/platform/meson_platform/mali_poweron_reg.h
+++ drivers/amlogic/mali/platform/meson_platform/mali_poweron_reg.h
@@ -28,17 +28,26 @@
 #define MALI_APB_GP_VSCL_START        0xd0060000
 #define MALI_APB_GP_VSCL_END          0xd0060004
 #define MALI_APB_GP_CMD               0xd0060020
-#define MALI_APB_GP_INT_STAT          0xd0060024
+#define MALI_APB_GP_INT_RAWSTAT       0xd0060024
 #define MALI_APB_GP_INT_CLEAR         0xd0060028
 #define MALI_APB_GP_INT_MASK          0xd006002c
+#define MALI_APB_GP_INT_STAT          0xd0060030
 
 #define MALI_MMU_DTE_ADDR             0xd0063000
 #define MALI_MMU_STATUS               0xd0063004
 #define MALI_MMU_CMD                  0xd0063008
+#define MALI_MMU_RAW_STATUS           0xd0064014
+#define MALI_MMU_INT_CLEAR            0xd0064018
+#define MALI_MMU_INT_MASK             0xd006401c
+#define MALI_MMU_INT_STATUS           0xd0064020
 
 #define MALI_PP_MMU_DTE_ADDR          0xd0064000
 #define MALI_PP_MMU_STATUS            0xd0064004
 #define MALI_PP_MMU_CMD               0xd0064008
+#define MALI_PP_MMU_RAW_STATUS        0xd0064014
+#define MALI_PP_MMU_INT_CLEAR         0xd0064018
+#define MALI_PP_MMU_INT_MASK          0xd006401c
+#define MALI_PP_MMU_INT_STATUS        0xd0064020
 
 #define MALI_APB_PP_REND_LIST_ADDR    0xd0068000
 #define MALI_APB_PP_REND_RSW_BASE     0xd0068004
